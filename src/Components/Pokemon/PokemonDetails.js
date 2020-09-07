656e4adc0d06489f1b299dcd578dbf2e4bc18067
bslzz <bishalkoid@gmail.com>
2020-09-07 11:56:22 +0300

    edited typos and styled components

--- src/Components/Pokemon/PokemonDetails.js
+++ src/Components/Pokemon/PokemonDetails.js
@@ -50,9 +50,9 @@ const PokemonDetails = (props) => {
     }
     if (PokemonDetails.loading) {
       return (
-        <div class="d-flex justify-content-center">
-          <div class="spinner-border text-info" role="status">
-            <span class="sr-only">Loading...</span>
+        <div className="d-flex justify-content-center">
+          <div className="spinner-border text-info" role="status">
+            <span className="sr-only">Loading...</span>
           </div>
         </div>
       );
--- src/Components/Pokemon/ShowData.js
+++ src/Components/Pokemon/ShowData.js
@@ -6,9 +6,9 @@ export const ShowData = () => {
   const PokemonLists = useSelector((state) => state.PokemonLists);
   if (PokemonLists.loading) {
     return (
-      <div class="d-flex justify-content-center">
-        <div class="spinner-border text-info" role="status">
-          <span class="sr-only">Loading...</span>
+      <div className="d-flex justify-content-center">
+        <div className="spinner-border text-info" role="status">
+          <span className="sr-only">Loading...</span>
         </div>
       </div>
     );
